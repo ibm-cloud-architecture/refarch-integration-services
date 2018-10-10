8aed81a8eb56b4573360d77d231cf9cf60544a25
Jerome Boyer <boyerje@us.ibm.com>
2018-10-09 21:58:45 -0700

    change deployment, values and tests

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -1,3 +1,4 @@
 #!/bin/bash -v
 echo "run integration tests"
-curl -X GET http://green.csplab.local/caseserv/api/v1/customers/1
+# curl -X GET http://green.csplab.local/caseserv/api/v1/customers/1
+curl -X GET http://172.16.40.133:30429/caseserv/api/v1/customers/1
